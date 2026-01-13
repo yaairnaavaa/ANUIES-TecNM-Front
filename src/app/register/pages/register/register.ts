f7402bdd784233e5132637e0c1d2dbc75ba76d22
Jorge Betancourt <joisbetancourtes@ittepic.edu.mx>
2026-01-13 12:24:12 -0700

    Avances en login y registro

--- src/app/register/pages/register/register.ts
+++ src/app/register/pages/register/register.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../navbar/navbar';
-import { Stepper } from '../../stepper/stepper';
 import { RegistrationForm } from '../../registration-form/registration-form';
 @Component({
   selector: 'app-register',
-  imports: [Navbar, Stepper, RegistrationForm],
+  imports: [Navbar, RegistrationForm],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  activeStep: number = 1;
 
-  goToNextStep() {
-    if (this.activeStep < 3) {
-      this.activeStep++;
-    }
-  }
-
-  goToPreviousStep() {
-    if (this.activeStep > 1) {
-      this.activeStep--;
-    }
-  }
 }
