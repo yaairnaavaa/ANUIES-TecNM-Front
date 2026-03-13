b0d8fc1e8cec50c6c1fc289183a91938d7f98534
Jorge Betancourt <joisbetancourtes@ittepic.edu.mx>
2026-03-13 11:07:15 -0700

    fix home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+  private authService = inject(AuthService);
+  menuItems = this.authService.menu;
 }
